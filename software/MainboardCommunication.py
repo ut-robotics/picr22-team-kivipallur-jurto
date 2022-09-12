095a34104fd0a53df37935d9b3b2faf321852766
Priit400 <112821827+Priit400@users.noreply.github.com>
2022-09-12 19:26:20 +0300

    Update MainboardCommunication.py
    eemaldasin testi printimised

--- software/MainboardCommunication.py
+++ software/MainboardCommunication.py
@@ -9,13 +9,9 @@ speed3 = -20
 thrower_speed = 00
 
 andmed = struct.pack('<hhhHBH', speed1, speed2, speed3, thrower_speed, disable_failsafe, 0xAAAA)
-print("Help1 \n")
 ser = serial.Serial('/dev/ttyACM0')  # open serial port
-print("Help2 \n")
 for i in range(50): #16V 500mA
     ser.write(andmed)     # write a string
-    print("Help3 \n")
     time.sleep(0.4)
-    #print(ser.readlines())
 
 ser.close()
