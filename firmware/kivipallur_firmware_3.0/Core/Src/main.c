74dfb38332e59a0296dba7b80d82f5c4a2b7a554
kaspar <kasparkoolis@gmail.com>
2023-01-18 20:32:21 +0200

    kustudus

--- firmware/kivipallur_firmware_3.0/Core/Src/main.c
+++ firmware/kivipallur_firmware_3.0/Core/Src/main.c
@@ -293,20 +293,15 @@ int main(void)
   /* USER CODE BEGIN 2 */
 
   HAL_TIM_PWM_Start(&htim15, TIM_CHANNEL_2); //Trhower
-  //HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2); //Mootor1
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1); //Mootor2
-  //HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
-  //HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_4); //Mootor3
 
-  HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_1 | TIM_CHANNEL_2);
-  //HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_2);
-  HAL_TIM_Encoder_Start(&htim4, TIM_CHANNEL_1 | TIM_CHANNEL_2);
-  //HAL_TIM_Encoder_Start(&htim4, TIM_CHANNEL_2);
-  HAL_TIM_Encoder_Start(&htim8, TIM_CHANNEL_1 | TIM_CHANNEL_2);
-  //HAL_TIM_Encoder_Start(&htim8, TIM_CHANNEL_2);
 
+  HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_1 | TIM_CHANNEL_2);
+  HAL_TIM_Encoder_Start(&htim4, TIM_CHANNEL_1 | TIM_CHANNEL_2);
+  HAL_TIM_Encoder_Start(&htim8, TIM_CHANNEL_1 | TIM_CHANNEL_2);
 
   TIM1->CCR2 = 0;
   TIM2->CCR1 = 0;
@@ -323,12 +318,6 @@ int main(void)
   TIM2->CCR3 = 0;
   TIM2->CCR4 = 20000;
 
-  TIM1->CCR1 = 0;
-  TIM1->CCR2 = 0;
-  TIM2->CCR1 = 0;
-  TIM2->CCR2 = 0;
-  TIM2->CCR3 = 0;
-  TIM2->CCR4 = 0;
 */
 
 
